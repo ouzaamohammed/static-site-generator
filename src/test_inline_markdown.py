690c8e2f2de3ea947041509db205c054c6ad70f5
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-08-18 19:06:06 +0100

    split markdown into blocks

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,14 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
+from inline_markdown import (
+    split_nodes_delimiter, 
+    extract_markdown_images, 
+    extract_markdown_links, 
+    split_nodes_image, 
+    split_nodes_link, 
+    text_to_textnodes
+)
 
 class TestMarkdownToTextNode(unittest.TestCase):
     def test_delim_code(self):
