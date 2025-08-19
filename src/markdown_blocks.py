7216fe6b0e4b14e14b405f13bd2e2aca3b6981ec
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-08-19 23:37:10 +0100

    add block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,55 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDEREDLIST = "unordered_list"
+    ORDEREDLIST = "ordered_list"
+
+def is_heading(block):
+    return block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### "))
+
+def is_code(block):
+    lines = block.split("\n")
+    return lines[0].startswith("```") and lines[-1].endswith("```")
+
+def is_quote(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith(">"):
+            return False
+    return True
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith("- "):
+            return False
+    return True
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+    for i in range(len(lines)):
+        if not lines[i].startswith(f"{i+1}. "):
+            return False
+    return True
+
+def block_to_block_type(block):
+    if is_heading(block):
+        return BlockType.HEADING
+    elif is_code(block):
+        return BlockType.CODE
+    elif is_quote(block):
+        return BlockType.QUOTE
+    elif is_unordered_list(block):
+        return BlockType.UNORDEREDLIST
+    elif is_ordered_list(block):
+        return BlockType.ORDEREDLIST
+    else:
+        return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,10 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    block_to_block_type, 
+    markdown_to_blocks, 
+    BlockType
+)
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,3 +49,35 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_paragraph(self):
+        block_type = block_to_block_type("this is a paragraph")
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_heading(self):
+        block_type = block_to_block_type("# this is a heading")
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_code(self):
+        block_type = block_to_block_type("```\nthis is a code\n```")
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_quote(self):
+        block_type = block_to_block_type(">this is a quote\n>another quote")
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        block_type = block_to_block_type("- this is a\n- unordered list")
+        self.assertEqual(block_type, BlockType.UNORDEREDLIST)
+
+    def test_unordered_list_false(self):
+        block_type = block_to_block_type("- this is a\n unordered list")
+        self.assertNotEqual(block_type, BlockType.UNORDEREDLIST)
+
+    def test_ordered_list(self):
+        block_type = block_to_block_type("1. this is an\n2. ordered list")
+        self.assertEqual(block_type, BlockType.ORDEREDLIST)
+
+    def test_ordered_list_false(self):
+        block_type = block_to_block_type("1. this is an\n3. ordered list")
+        self.assertNotEqual(block_type, BlockType.ORDEREDLIST)
