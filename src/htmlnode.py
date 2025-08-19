cab6845c96b2aacc0149eb81871a9917a9c2d109
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-08-19 23:47:14 +0100

    fix html node error

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,7 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplemented
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
         if self.props is None:
