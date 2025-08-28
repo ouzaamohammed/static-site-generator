dda06fdc1b2982b8de7428f89fb41161ec1f3ea5
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-08-28 11:53:31 +0100

    docs: add README

---
+++ README.md
@@ -0,0 +1,37 @@
+# Static Site Generator
+
+A simple static site generator built with Python.
+
+# Motivation / why
+
+Learning exercise to understand how static site generators like [Hugo](https://gohugo.io/) work under the hood.
+
+# Features
+
+- Converts Markdown to HTML
+- Copy static files to `/docs` directory
+- Deploys the final generated site to github pages
+
+# Usage
+
+Clone the repository
+
+```bash
+git clone https://github.com/ouzaamohammed/static-site-generator
+cd static-site-generator
+```
+Open the project inside your text editor of choice.
+
+Create your Markdown pages inside `/content` directory, and copy your assets in `/static` directory.
+
+Run these commands
+
+```bash
+chmod +x main.sh
+./main.sh
+```
+
+This should generate your site in `/docs` directory and copy all assets from `/content`.
+
+Visit http://localhost:8888/ to see your site.
+
--- src/main.py
+++ src/main.py
@@ -6,22 +6,24 @@ from copy_files import copy_files
 from gen_content import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_docs = "./docs"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
-    print("deleting docs directory...")
-    if os.path.exists(dir_path_docs):
-        shutil.rmtree(dir_path_docs)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    print("deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print("copying static files to docs directory...")
-    copy_files(dir_path_static, dir_path_docs)
-
-    basepath = sys.argv[1] or "/"
+    print("copying static files to public directory...")
+    copy_files(dir_path_static, dir_path_public)
 
     print("generating all pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
